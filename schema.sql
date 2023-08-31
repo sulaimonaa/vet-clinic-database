3cb5f8f023d23149a9ab433c837b5d7b68137aaf
Sulaimon Azeez Ayodeji <sulaimonaa@gmail.com>
2023-08-31 17:37:54 +0100

    update data, schema, queries file

--- schema.sql
+++ schema.sql
@@ -10,3 +10,20 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD species CHAR(50);
+
+CREATE TABLE owners (id INT AUTO_INCREMENT PRIMARY KEY, full_name VARCHAR(100), age INT);
+
+CREATE TABLE species (id INT AUTO_INCREMENT PRIMARY KEY, name CHAR(50));
+
+ALTER TABLE animals MODIFY COLUMN id INT AUTO_INCREMENT;
+
+ALTER TABLE animals DROP species;
+
+ALTER TABLE animals ADD COLUMN species_id INT;
+
+ALTER TABLE animals ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species(id);
+
+ALTER TABLE animals ADD COLUMN owners_id INT;
+
+ALTER TABLE animals ADD CONSTRAINT fk_owners FOREIGN KEY (owners_id) REFERENCES owners(id);
+
